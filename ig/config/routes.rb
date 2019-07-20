ef0f6554f506c44759433ed1efa85c66844111e0
Ge Tadeo <mr.genesis.tadeo@gmail.com>
2019-07-20 20:10:23 +0800

    Add edit profile page

--- ig/config/routes.rb
+++ ig/config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   root to: 'home#index'
 
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
